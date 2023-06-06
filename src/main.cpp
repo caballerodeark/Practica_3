a9b330ea62f057f6eb42824aaa97c7d48b985ce7
caballerodeark <126196836+caballerodeark@users.noreply.github.com>
2023-06-06 18:44:02 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,28 @@
 #include <Arduino.h>
 
-void ledoff(void * parameter)
-  {
-    bool t=false;
-  /* loop forever */
-  for(;;){
-    //if(t){
-    digitalWrite(14,LOW);
-    Serial.println("apagado");
-    delay (500);
+//Tarea de encender
+void encenderTask(void * parameter)
+{
+    for(;;)
+    {
+        digitalWrite(23,HIGH);
+        delay(1000);
     }
-  //  else t=true;
-  //}
+}
+//Tarea de apagar
+void apagarTask(void * parameter)
+{
+    delay(500);
+    for(;;)
+    {
+        digitalWrite(23,LOW);
+        delay(1000);
+    }
 }
 
 void setup()
 {
-  Serial.begin(112500);
-  pinMode(14,OUTPUT);
-  xTaskCreate(
-  ledoff, 
-  "apaga el led",
-  10000, /* Stack size of task */
-  NULL, /* parameter of the task */
-  1, /* priority of the task */
-  NULL); /* Task handle to keep track of created task */
-}
-void loop()
-{
-  digitalWrite(14,HIGH);
-    Serial.println("encendido");
-
-  delay(1000);
-
+    Serial.begin(112500);
+    xTaskCreate(encenderTask,"Encender Led",10000,NULL,0,NULL);
+    xTaskCreate(apagarTask,"Apagar Led",10000,NULL,0,NULL);
 }
